c2565b6ad1e44b462e99b121c2ff4b24d1b4cfea
Eddie Pace <epace@scottlogic.com>
2022-06-20 16:41:37 +0100

    test getting spells dynamically

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
     import Sheet from "./components/sheet/Sheet.svelte";
-import { TEST_CHARACTER } from "./data/data";
+    import { TEST_CHARACTER } from "./data/data";
+    let spellData = "";
+
+    async function getSpellData() {
+        const response = await fetch('https://5e.tools/spells.html');
+        const body = await response.text();
+        console.log(body);
+        spellData = body;
+    }
 </script>
 
 <main>
     <Sheet character={TEST_CHARACTER}/>
+    <div>
+        <h2>Spell Finder</h2>
+        <button on:click={getSpellData}>GET SPELLS</button>
+        <div>
+            {spellData}
+        </div>
+    </div>
 </main>
